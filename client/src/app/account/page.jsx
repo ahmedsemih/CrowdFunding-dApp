e6cbf972b28fdcb9a824607b684e44f72b4be5b1
Ahmed Semih Erkan <ahmedsemiherkan@gmail.com>
2024-01-23 19:19:50 +0300

    fix signer address issue

--- client/src/app/account/page.jsx
+++ client/src/app/account/page.jsx
@@ -1,33 +1,56 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
 import { Card } from "@/components";
-import connectBlockchain from "@/utils/connectBlockchain";
-import React from "react";
+import Logo from '/public/Logo.png';
+import { useEthersContext } from "@/contexts/EthersContext";
 
-const fetchCampaignsByUser = async (address) => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/campaigns?owner=${address}`,
-    { method: "GET", cache: "no-cache" }
-  );
-  const data = await res.json();
-  var campaigns = data.campaigns;
+const Account = () => {
+  const { signer } = useEthersContext();
+  const [campaigns, setCampaigns] = useState(null);
+  const [totalCollected, setTotalCollected] = useState(0);
+  const [totalWithdrawn, setTotalWithdrawn] = useState(0);
 
-  if (!campaigns) campaigns = [];
+  useEffect(() => {
+    const fetchCampaigns = async () => {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/campaigns?owner=${signer?.address}`
+      );
+      const data = await res.json();
+      var campaigns = data.campaigns;
 
-  const totalCollected = campaigns.reduce(
-    (acc, campaign) => acc + Number(campaign.collectedAmount),
-    0
-  );
-  const totalWithdrawn = campaigns.reduce(
-    (acc, campaign) => acc + Number(campaign.withdrawedAmount),
-    0
-  );
+      if (!campaigns) campaigns = [];
 
-  return { campaigns: campaigns, totalCollected, totalWithdrawn };
-};
+      const totalCollected = campaigns.reduce(
+        (acc, campaign) => acc + Number(campaign.collectedAmount),
+        0
+      );
+      const totalWithdrawn = campaigns.reduce(
+        (acc, campaign) => acc + Number(campaign.withdrawedAmount),
+        0
+      );
 
-const Account = async () => {
-  const { signer } = connectBlockchain();
-  const { campaigns, totalCollected, totalWithdrawn } =
-    await fetchCampaignsByUser(signer.address);
+      setCampaigns(campaigns);
+      setTotalCollected(totalCollected);
+      setTotalWithdrawn(totalWithdrawn);
+    };
+
+    signer?.address && fetchCampaigns();
+  }, [signer?.address]);
+
+  if (!signer?.address || campaigns === null)
+    return (
+      <div className="w-full h-[90%] flex justify-center items-center z-10">
+        <div className="animate-pulse flex flex-col items-center gap-2">
+          <Image src={Logo} alt="fundseed" width={112} height={112} />
+          <p className="text-2xl text-emerald-500 text-center font-bold mt-2">
+            Loading..
+          </p>
+        </div>
+      </div>
+    );
 
   return (
     <main>
@@ -35,7 +58,7 @@ const Account = async () => {
         <h1 className="text-2xl font-semibold">Account</h1>
         <div className="flex flex-col my-4">
           <span className="text-neutral-400">Address:</span>
-          <span className="text-neutral-300 truncate">{signer.address}</span>
+          <span className="text-neutral-300 truncate">{signer?.address}</span>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 items-center justify-between gap-4">
           <div className="flex flex-col gap-2">
@@ -44,11 +67,11 @@ const Account = async () => {
           </div>
           <div className="flex flex-col gap-2">
             <span className="text-neutral-400">Total Collected:</span>
-            <span className="text-neutral-300">{totalCollected ?? 0}</span>
+            <span className="text-neutral-300">{totalCollected}</span>
           </div>
           <div className="flex flex-col gap-2">
             <span className="text-neutral-400">Total Withdrawn:</span>
-            <span className="text-neutral-300">{totalWithdrawn ?? 0}</span>
+            <span className="text-neutral-300">{totalWithdrawn}</span>
           </div>
         </div>
       </div>
@@ -75,7 +98,7 @@ const Account = async () => {
                 <Card
                   campaign={campaign}
                   key={campaign.id}
-                  user={signer.address}
+                  user={signer?.address}
                 />
               ))}
             </div>
